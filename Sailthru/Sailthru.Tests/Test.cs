e4d4d1746a70c0af1c3cc975a714ca62e2d95d55
Kirill Vainer <kvainer@sailthru.com>
2020-09-14 14:00:48 -0400

    [DEVOPS-245] add test project

---
+++ Sailthru/Sailthru.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Sailthru.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
